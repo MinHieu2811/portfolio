0538a48f8889065ec3d310bf5ba89e3f9efd5a9a
Minh Hieu Nguyen <hieu.nm@crossian.com>
2023-06-16 11:20:24 +0700

    update image SEO

--- components/head.js
+++ components/head.js
@@ -1,5 +1,4 @@
 import Head from "next/head"
-import image_SEO from "../public/images/image_SEO.png"
 
 export const MyHead = () => {
     return (
@@ -8,7 +7,7 @@ export const MyHead = () => {
             <meta property="og:title" content="Minh Hieu: Passionate Web Developer"></meta>
             <meta property="og:site_name" content="Minh Hieu's Portfolio"></meta>
             <meta property="og:description" content="Minh Hieu: Passionate web developer dedicated to crafting exceptional online experiences."></meta>
-            <meta property="og:image" content={image_SEO}></meta>
+            <meta property="og:image" content="/image_SEO.png"></meta>
         </Head>
     )
 }
